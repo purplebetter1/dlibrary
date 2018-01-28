f48b006963102732bfa1e82dc2c4c865c3146ef6
Andrew Grablewski <booleanorama@gmail.com>
2018-01-28 15:46:31 -0500

    list and detail views added

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -22,3 +22,19 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genres':num_genres},
     )
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+	model = Author
+	paginate_by = 5
+
+class AuthorDetailView(generic.DetailView):
+	model = Author
